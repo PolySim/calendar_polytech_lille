bd9895f74566f67318a051aa95362adcca73489b
Desdevises Simon <simon.desdevises@gmail.com>
2024-09-01 22:21:54 +0200

    feat : select click outside

--- src/components/ui/selectLink.vue
+++ src/components/ui/selectLink.vue
@@ -1,38 +1,47 @@
 <template>
-  <div
-    class="w-60 relative bg-white rounded-md border border-solid border-black"
-  >
+  <div class="relative w-60 h-10">
     <div
-      :class="
-        cn('flex justify-between w-full cursor-pointer py-2 px-4', {
-          'border-b border-solid border-gray-200': isOpen,
-        })
-      "
-      @click="isOpen = !isOpen"
+      class="w-60 absolute top-0 left-0 bg-white rounded-md border border-solid border-black overflow-hidden"
     >
-      <div>{{ value }}</div>
-      <Icon
-        :class="cn('transition', { 'rotate-180': isOpen, 'rotate-0': !isOpen })"
-        :size="24"
-        name="ic:round-keyboard-arrow-down"
-      />
-    </div>
-    <div v-show="isOpen" class="flex flex-col max-h-60 overflow-y-auto">
-      <NuxtLink
-        v-for="option in options"
-        :key="option.label"
-        :to="option.to"
-        class="py-2 px-4 hover:bg-gray-200 cursor-pointer transition"
+      <div
+        ref="clickOutsideContainer"
+        :class="
+          cn('flex justify-between w-full cursor-pointer py-2 px-4', {
+            'border-b border-solid border-gray-200': isOpen,
+          })
+        "
+        @click="isOpen = !isOpen"
       >
-        {{ option.label }}
-      </NuxtLink>
+        <div>{{ value }}</div>
+        <Icon
+          :class="
+            cn('transition', { 'rotate-180': isOpen, 'rotate-0': !isOpen })
+          "
+          :size="24"
+          name="ic:round-keyboard-arrow-down"
+        />
+      </div>
+      <div v-show="isOpen" class="flex flex-col max-h-60 overflow-y-auto">
+        <NuxtLink
+          v-for="option in options"
+          :key="option.label"
+          :to="option.to"
+          class="py-2 px-4 hover:bg-gray-200 cursor-pointer transition"
+        >
+          {{ option.label }}
+        </NuxtLink>
+      </div>
     </div>
   </div>
 </template>
 
 <script lang="ts" setup>
 import { cn } from "~/src/utils/cn";
+import { onClickOutside } from "@vueuse/core";
 import { ref } from "vue";
+
+const clickOutsideContainer = ref<HTMLDivElement | null>(null);
+onClickOutside(clickOutsideContainer, (event) => (isOpen.value = false));
 
 const props = defineProps<{
   value: string;
