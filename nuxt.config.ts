db0bdcb85be35eac42e4dfaf72a2fd9d9579fcda
Desdevises Simon <simon.desdevises@gmail.com>
2024-09-03 13:18:14 +0200

    feat : add shadcn

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,23 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss", "@nuxt/eslint", "@nuxt/icon"],
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "@nuxt/eslint",
+    "@nuxt/icon",
+    "shadcn-nuxt",
+  ],
+  shadcn: {
+    /**
+     * Prefix for all the imported component
+     */
+    prefix: '',
+    /**
+     * Directory that the component lives in.
+     * @default "./components/ui"
+     */
+    componentDir: './src/components/ui'
+  },
   runtimeConfig: {
     BASE_URL: process.env.BASE_URL,
     public: {
